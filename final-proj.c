090c5121665ca75e3450f8cd451631eeaaf0df39
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-04-04 19:48:29 +0300

    refactor: split main function into smaller functions and limit student array size
    - Define MAX_STUDENTS constant and use it to limit the size of the students array
    - Split the main function into smaller functions
    - Each function now has a single responsibility

--- final-proj.c
+++ final-proj.c
@@ -4,20 +4,16 @@
 
 #include "functions.h"
 
-int main(void)
+#define MAX_STUDENTS 1000
+
+void getAndPrintStudentInfo(struct student *students, int numStudent)
 {
-	int numStudent = 0;
-	printf("How many students?\n");
-	scanf("%d", &numStudent);
-	assert(numStudent <= 1000);
-
-	struct student students[numStudent];
 	getInfo(students, numStudent);
-
 	printInfo(students, numStudent);
+}
 
-	writeInfo(students, numStudent, "./student_list.txt");
-
+void calculateAndPrintStats(struct student *students, int numStudent)
+{
 	float maxAvg = highestGPA(students, numStudent);
 	float minAvg = lowestGPA(students, numStudent);
 	float maxBP = highestBP(students, numStudent);
@@ -25,19 +21,36 @@ int main(void)
 	printf("\nThe highest average GPA is: %.2f\n", maxAvg);
 	printf("The lowest average GPA is: %.2f\n", minAvg);
 	printf("The highest BP GPA is: %.2f\n", maxBP);
+}
 
+void printStudentDetails(struct student *students, int numStudent)
+{
 	printLastName(students, numStudent);
-
 	printOldest(students, numStudent);
-
 	printYoungest(students, numStudent);
+}
 
-	searchID(students, numStudent);
-
+void sortAndPrint(struct student *students, int numStudent)
+{
 	printf("\nSorted list of student's GPA in descending order\n");
 	sortDescend(students, 0, numStudent - 1);
-
 	printInfo(students, numStudent);
+}
+
+int main(void)
+{
+	int numStudent = 0;
+	printf("How many students?\n");
+	scanf("%d", &numStudent);
+	assert(numStudent <= MAX_STUDENTS);
+
+	struct student students[MAX_STUDENTS];
+	getAndPrintStudentInfo(students, numStudent);
+	writeInfo(students, numStudent, "./student_list.txt");
+	calculateAndPrintStats(students, numStudent);
+	printStudentDetails(students, numStudent);
+	searchID(students, numStudent);
+	sortAndPrint(students, numStudent);
 
 	return 0;
 }
